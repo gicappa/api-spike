f97229ac701bc856a359eff805d0f554edf3f9eb
Michele Cantelli <emmekappa@gmail.com>
2013-07-09 18:09:58 +0200

    Implementing jersey...

--- src/main/java/jobengine/api/Result.java
+++ src/main/java/jobengine/api/Result.java
@@ -1,11 +1,18 @@
 package jobengine.api;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 public class Result {
     static enum Status {
         ok, ko, unauth
     }
 
     public Status status;
+
+    @XmlElement
+    public String Name;
 
     public Object payload;
 }
