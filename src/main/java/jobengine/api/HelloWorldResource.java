2894877657457b56c93aed82ebfcfd92214890d7
Michele Cantelli <emmekappa@gmail.com>
2013-07-09 18:45:53 +0200

    Implementing jersey...

--- src/main/java/jobengine/api/HelloWorldResource.java
+++ src/main/java/jobengine/api/HelloWorldResource.java
@@ -6,10 +6,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hi")
-public class HelloWorldAPI {
+public class HelloWorldResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String sayPlainTextHello() {
         return "Hello Jersey";
+    }
+
+    @GET
+    @Path("/mah")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Result hello() {
+        return new Result();
     }
 }
