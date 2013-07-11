674f54b0d5242168db6b11eb7daf68239895c795
Gian Carlo Pace <giancarlo.pace@gmail.com>
2013-07-11 17:43:45 +0200

    switched to jetty from grizzly to load web.xml for testing as well

--- src/test/java/jobengine/application/api/RestfulAdverts.java
+++ src/test/java/jobengine/application/api/RestfulAdverts.java
@@ -6,7 +6,7 @@ import com.eclipsesource.restfuse.Method;
 import com.eclipsesource.restfuse.Response;
 import com.eclipsesource.restfuse.annotation.Context;
 import com.eclipsesource.restfuse.annotation.HttpTest;
-import jobengine.application.utils.GrizzlyServer;
+import jobengine.application.utils.WebServer;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.runner.RunWith;
@@ -16,11 +16,11 @@ import static com.eclipsesource.restfuse.Assert.assertOk;
 @RunWith(HttpJUnitRunner.class)
 public class RestfulAdverts {
 
-    @ClassRule public static GrizzlyServer g = new GrizzlyServer(Adverts.class);
-    @Rule public Destination d = new Destination(this, "http://localhost:8080/api-spike/");
+    @ClassRule public static WebServer g = new WebServer();
+    @Rule public Destination d = new Destination(this, "http://localhost:8080/rest/");
     @Context private Response response;
 
-    @HttpTest(method = Method.GET, path = "/adverts")
+    @HttpTest(method = Method.GET, path = "/helloworld")
     public void checkRestfuseOnlineStatus() {
         assertOk(response);
     }
---
+++ src/test/java/jobengine/application/utils/WebServer.java
@@ -0,0 +1,48 @@
+package jobengine.application.utils;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.junit.rules.ExternalResource;
+
+import static java.lang.System.err;
+import static java.lang.System.exit;
+import static java.lang.System.out;
+
+public class WebServer extends ExternalResource {
+
+    private Server server;
+
+    public WebServer() {
+        try {
+            server = new Server(8080);
+            server.setHandler(createWebAppContext());
+            server.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+            exit(-1);
+        }
+    }
+
+    private WebAppContext createWebAppContext() {
+        WebAppContext wac = new WebAppContext();
+        wac.setResourceBase("src/main/webapp");
+        wac.setDescriptor("src/main/webapp/WEB-INF/web.xml");
+        wac.setContextPath("/");
+        wac.setParentLoaderPriority(true);
+        return wac;
+    }
+
+    protected void before() throws Throwable {
+        out.println("starting webserver...");
+        server.start();
+    }
+
+    protected void after() {
+        try {
+            server.stop();
+            out.println("...stopped webserver");
+        } catch (Exception e) {
+            err.println("problem while stopping webserver");
+        }
+    }
+}
