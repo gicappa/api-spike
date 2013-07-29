56b4b4f3a08bfd3acf1af6da568d212aab80efa1
Gian Carlo Pace <giancarlo.pace@gmail.com>
2013-07-29 23:22:19 +0200

    Fixed a problem on versioning

--- src/main/java/jobengine/application/ApiAdverts.java
+++ src/main/java/jobengine/application/ApiAdverts.java
@@ -12,27 +12,26 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/adverts",
-        headers = {"Accept=application/vnd.jobrapido.v1+json", "Accept=application/json"})
+@RequestMapping(value = "/adverts", produces = {"application/json", "application/vnd.jobrapido.v1+json"})
 class ApiAdverts {
 
     private Logger logger = LoggerFactory.getLogger(ApiAdverts.class);
     @Autowired
     private Adverts adverts;
 
-    @RequestMapping(method = RequestMethod.GET, headers = {"Accept=application/vnd.jobrapido.alpha+json"})
-    @ResponseStatus(HttpStatus.OK)
-    @ResponseBody
-    public List<Advert> index_alpha(String what, String where) {
-        logger.debug("api|alpha|GET /adverts");
-        return adverts.search(what, where);
-    }
-
     @RequestMapping(method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public List<Advert> index_v1(String what, String where) {
         logger.debug("api|v1|GET /adverts");
+        return adverts.search(what, where);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/vnd.jobrapido.alpha+json")
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public List<Advert> index_alpha(String what, String where) {
+        logger.debug("api|alpha|GET /adverts");
         return adverts.search(what, where);
     }
 
