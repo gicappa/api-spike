c18b31b122439f153871d9ae9f0d73fda93577a3
Gian Carlo Pace <giancarlo.pace@gmail.com>
2013-08-01 10:04:08 +0200

    Refactored code

--- src/main/java/jobengine/application/RestHeaderInterceptor.java
+++ src/main/java/jobengine/application/RestHeaderInterceptor.java
@@ -10,10 +10,7 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.Enumeration;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -30,44 +27,33 @@ public class RestHeaderInterceptor extends HandlerInterceptorAdapter {
 
         response.setHeader("Server", "Jobrapido");
         response.addHeader("Access-Control-Allow-Origin", "*");
-
         response.setDateHeader("Date", new Date().getTime());
-        String accept = request.getHeader("Accept");
         response.setContentType("application/json;charset=utf-8");
-
-        if (accept == null ) {
-            response.addHeader("X-Jobrapido-Media-Type", "v1");
-            return true;
-        }
-        Pattern p = Pattern.compile("vnd\\.jobrapido\\.(.*)\\+(.*)");
-        Matcher m = p.matcher(MediaType.parseMediaType(accept).getSubtype());
-        if (accept == null || m.matches()) {
-            String version = m.group(1);
-            System.out.println(version);
-            response.addHeader("X-Jobrapido-Media-Type", version);
-        } else {
-            response.addHeader("X-Jobrapido-Media-Type", "v1");
-        }
+        response.addHeader("X-Jobrapido-Media-Type", extractVersionFrom(request));
 
         return true;
+    }
+
+    private String extractVersionFrom(HttpServletRequest request) {
+        return extractVersionFrom(regexMatcherOf(acceptHeaderIn(request)));
+    }
+
+    private Matcher regexMatcherOf(String accept) {
+        String subtype = MediaType.parseMediaType(accept).getSubtype();
+        return Pattern.compile("vnd\\.jobrapido\\.(.*)\\+(.*)").matcher(subtype);
+    }
+
+    private String extractVersionFrom(Matcher m) {
+        return m.matches() ? m.group(1) : "v1";
+    }
+
+    private String acceptHeaderIn(HttpServletRequest request) {
+        return request.getHeader("Accept") == null ? "~/~" : request.getHeader("Accept");
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
         response.setContentType("application/json;charset=utf-8");
-    }
-
-    class Pippo implements Function<String, String> {
-
-        @Override
-        public String apply(@Nullable java.lang.String input) {
-            return null;
-        }
-
-        @Override
-        public boolean equals(@Nullable java.lang.Object object) {
-            return false;
-        }
     }
 
     private void logDebugInformationsOn(HttpServletRequest request) {
@@ -78,6 +64,4 @@ public class RestHeaderInterceptor extends HandlerInterceptorAdapter {
             }
         }
     }
-
-
 }
